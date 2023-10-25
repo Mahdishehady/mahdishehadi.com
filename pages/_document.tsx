d1a1db61a61375992246c83bca63767e3663a6c7
Mahdishehady <shehadi2000@gmail.com>
2023-10-25 18:13:38 +0300

    new commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
     render() {
@@ -6,6 +7,13 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+                    <Script type="text/javascript">
+        {`(function(c,l,a,r,i,t,y){
+          c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "jg5g9jg4pr");`}
+      </Script>
                 </Head>
                 <body>
                 <Main />
