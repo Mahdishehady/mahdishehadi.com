23fe277f5b330603754383af7864bd797204c682
Mahdishehady <shehadi2000@gmail.com>
2023-10-25 18:18:53 +0300

    clarity added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,13 +7,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
-                    <Script type="text/javascript">
-        {`(function(c,l,a,r,i,t,y){
-          c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "jg5g9jg4pr");`}
-      </Script>
+                    
                 </Head>
                 <body>
                 <Main />
