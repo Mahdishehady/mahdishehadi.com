e1d3ef1f2ac13a131e236cfb079d6b06e31752e3
Mahdishehady <shehadi2000@gmail.com>
2023-10-25 18:11:07 +0300

    adding clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-     
+
       <Script async src="https://www.googletagmanager.com/gtag/js?id=gtag('config', 'G-EM01G3PYRV')" />
       <Script id="google-analytics">
         {`
@@ -17,8 +17,15 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', 'G-EM01G3PYRV');
         `}
       </Script>
+      <Script type="text/javascript">
+        {`(function(c,l,a,r,i,t,y){
+          c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "jg5g9jg4pr");`}
+      </Script>
 
-     
+
       <Component {...pageProps} />
     </>
   );
