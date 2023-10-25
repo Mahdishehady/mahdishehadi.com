89e567867b569c2194d26255a96e982f49175275
Mahdishehady <shehadi2000@gmail.com>
2023-10-25 18:24:24 +0300

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,13 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', 'G-EM01G3PYRV');
         `}
       </Script>
-      <Script type="text/javascript">
-        {`(function(c,l,a,r,i,t,y){
-          c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "jg5g9jg4pr");`}
-      </Script>
+      
       
 
 
