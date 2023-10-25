71284260fdce53d90269d7bb8dbaef11e504f2f5
Mahdishehady <shehadi2000@gmail.com>
2023-10-25 12:43:11 +0300

    new commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'; // Import the Head component
 import Script from 'next/script';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-
-  useEffect(() => {
-    if(typeof window === undefined) return;
-    (window as any).dataLayer = (window as any).dataLayer || [];
-    function gtag(...rest:any){(window as any).dataLayer.push(rest);}
-    gtag('js', new Date());
-    gtag('config', 'G-EM01G3PYRV');
-  })
-
   return (
     <>
-      <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=gtag('config', 'G-EM01G3PYRV')" ></script>
+     
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=gtag('config', 'G-EM01G3PYRV')" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-EM01G3PYRV');
+        `}
+      </Script>
 
-      </Head>
+     
       <Component {...pageProps} />
     </>
   );
